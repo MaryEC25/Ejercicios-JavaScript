027b71305c9f2aeab3e179451edb310ca619f0a3
Mariana Encarnacion <marianaec07@gmail.com>
2025-05-23 23:57:21 -0600

    Feat: se agrega el ejercicio 2

---
+++ src/js/ejercicio2.js
@@ -0,0 +1,15 @@
+/*
+Exercise #2 (sugerencia map())
+Write a function that:
+●	Takes in an array of numbers.
+●	Doubles the value of each number in the array.
+●	Prints out the new updated array.
+Example: Given an array [1, 2, 4, 5]. The output should be [2, 4, 8, 10]
+*/
+
+const printDoubledNumbers = () =>{
+    const arrayNumbers = [1, 2, 4, 5];
+    const doubledNumbers = arrayNumbers.map(numero => numero * 2);
+    console.log(doubledNumbers);
+}
+printDoubledNumbers();
